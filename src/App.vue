3337a66be9639c1e615bd34f27c8b3ead621df0d
abdo42wa <abdo.ahmed.aa754@gmail.com>
2025-06-23 16:48:11 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import MainHeader from './components/MainHeader.vue'
+import MainFooter from './components/MainFooter.vue'
+</script>
+
+<template>
+  <div class="bg-[#F3F4F6] h-lvh">
+    <MainHeader />
+    <RouterView />
+    <MainFooter />
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { createApp } from 'vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
+import './index.css'
+import App from './App.vue'
+import Home from './views/Home.vue'
+import About from './views/About.vue'
+import Checkout from './views/Checkout.vue'
+
+const routes = [
+    { path: '/', name: 'home', component: Home },
+    { path: '/about', name: 'about', component: About },
+    { path: '/checkout', name: 'checkout', component: Checkout }
+]
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes,
+})
+
+createApp(App).use(router).mount('#app')
